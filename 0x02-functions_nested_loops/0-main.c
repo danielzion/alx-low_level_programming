b1fc9f691de523969d439d3e68b193e91f8c4070
Olaniyan Daniel Olusegun <danielolaniyan1@gmail.com>
2022-06-08 17:10:36 +0100

    Update 0-main.c

--- 0x02-functions_nested_loops/0-main.c
+++ 0x02-functions_nested_loops/0-main.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
